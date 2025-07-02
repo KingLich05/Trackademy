9860295b259290d03b70758d029dbd834224d306
Sultan <sultan.kurmanov@hardcode.kz>
2025-07-02 17:12:07 +0500

    asdf

---
+++ Trackademy.Application/Shared/BaseCrudController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Trackademy.Application.Shared;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BaseCrudController<T, TDto> : ControllerBase
+    where T : class
+    where TDto : class
+{
+    
+}
