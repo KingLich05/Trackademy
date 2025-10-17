7a0858880a35348b6dae025cc4d58f553cd36f56
Sultan <sultan.kurmanov@hardcode.kz>
2025-10-17 23:16:28 +0500

    task-52

--- Trackademy.Application/OrganizationServices/OrganizationService.cs
+++ Trackademy.Application/OrganizationServices/OrganizationService.cs
@@ -18,14 +18,13 @@ public class OrganizationService :
 
     public override async Task<Guid> CreateAsync(OrganizationAddModel dto)
     {
-        // Валидация уникальности адреса - приводим контекст к правильному типу
         var trackademyContext = (TrackademyDbContext)_context;
         var existingOrganization = await trackademyContext.Organizations
             .FirstOrDefaultAsync(o => o.Address.ToLower() == dto.Address.ToLower());
         
         if (existingOrganization != null)
         {
-            throw new ConflictException("данный адрес уже имеется в базу");
+            throw new ConflictException("Данный адрес уже имеется в базу");
         }
 
         return await base.CreateAsync(dto);
