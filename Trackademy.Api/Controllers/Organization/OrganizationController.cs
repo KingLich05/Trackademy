b8de3fce49f1d01185b35bd5cc2a02adcd96abec
Sultan <sultan.kurmanov@hardcode.kz>
2025-10-20 13:37:58 +0500

    fix organization

--- Trackademy.Api/Controllers/Organization/OrganizationController.cs
+++ Trackademy.Api/Controllers/Organization/OrganizationController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Trackademy.Api.BaseController;
 using Trackademy.Application.OrganizationServices;
 using Trackademy.Application.OrganizationServices.Models;
@@ -7,4 +8,11 @@ namespace Trackademy.Api.Controllers.Organization;
 
 [Authorize(Roles = "Admin")]
 public class OrganizationController(IOrganizationService service) :
-    BaseCrudController<Domain.Users.Organization, OrganizationDto, OrganizationAddModel, OrganizationAddModel>(service);
+    BaseCrudController<Domain.Users.Organization, OrganizationDto, OrganizationAddModel, OrganizationAddModel>(service)
+{
+    [AllowAnonymous]
+    public override async Task<IActionResult> GetAll()
+    {
+        return await base.GetAll();
+    }
+}
