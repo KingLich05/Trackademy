968ce1e205db17be3d8151a789be11571d1e7900
Sultan <sultan.kurmanov@hardcode.kz>
2025-10-23 00:27:33 +0500

    add Migration fix assignment

---
+++ Trackademy.Application/Persistance/migrations/20251022192617_FixAssignment.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trackademy.Application.Persistance.migrations
+{
+    /// <inheritdoc />
+    public partial class FixAssignment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assignments_Subjects_SubjectId",
+                table: "Assignments");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Assignments");
+
+            migrationBuilder.RenameColumn(
+                name: "SubjectId",
+                table: "Assignments",
+                newName: "GroupId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Assignments_SubjectId",
+                table: "Assignments",
+                newName: "IX_Assignments_GroupId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Assignments",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(800)",
+                oldMaxLength: 800,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Assignments",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assignments_Groups_GroupId",
+                table: "Assignments",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assignments_Groups_GroupId",
+                table: "Assignments");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Assignments");
+
+            migrationBuilder.RenameColumn(
+                name: "GroupId",
+                table: "Assignments",
+                newName: "SubjectId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Assignments_GroupId",
+                table: "Assignments",
+                newName: "IX_Assignments_SubjectId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Assignments",
+                type: "character varying(800)",
+                maxLength: 800,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Assignments",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assignments_Subjects_SubjectId",
+                table: "Assignments",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
