b518220e3fbb5878ac7ed78658ed28f2227e66b6
Sultan <sultan.kurmanov@hardcode.kz>
2025-11-09 02:22:07 +0500

    add migration in user

--- Trackademy.Application/Persistance/Configuration/UserConfig.cs
+++ Trackademy.Application/Persistance/Configuration/UserConfig.cs
@@ -11,7 +11,6 @@ public class UserConfig: IEntityTypeConfiguration<User>
         b.HasKey(x => x.Id);
         b.Property(x => x.FullName).IsRequired();
         b.Property(x => x.Login).IsRequired();
-        b.Property(x => x.Email).IsRequired();
         b.Property(x => x.PasswordHash).IsRequired();
         b.Property(x => x.CreatedDate).HasColumnType("timestamptz");
 
